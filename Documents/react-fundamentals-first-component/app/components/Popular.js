ebe5e7b5c8a4f30e5bbd6abe5bd5ccf8c28f9e70
Piyush Vijay <piyushvijay.1997@gmail.com>
2017-05-13 12:32:42 +0530

    repo listing in popular.js

--- Documents/react-fundamentals-first-component/app/components/Popular.js
+++ Documents/react-fundamentals-first-component/app/components/Popular.js
@@ -1,5 +1,37 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('./api/api');
+
+function RepoGrids(props){
+	return(
+		<ul className="popular-list">
+			{props.repos.map(function(repo, index){
+				return(
+					<li key={repo.name} className="popular-item">
+						<div className="popular-rank">
+							#{index+1}
+						</div>
+						<ul className="space-list-item">
+							<li>
+								<img
+								className="avatar" 
+								src={repo.owner.avatar_url} 
+								alt={'Avatar for ' + repo.ownerlogin} />
+							</li>
+							<li><a href={repo.html_url}>{repo.name}</a></li>
+							<li>@{repo.owner.login}</li>
+							<li>{repo.stargazers_count}</li>
+						</ul>
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
+
+RepoGrids.propTypes = {
+	repos: PropTypes.array.isRequired,
+}
 
 function SelectLanguage(props){
 	var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -29,22 +61,34 @@ class Popular extends React.Component{
 	constructor (props){
 		super(props);
 		this.state = {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		}
 
 		this.updateLanguage = this.updateLanguage.bind(this);
-	};
+	}
+	componentDidMount (){
+		this.updateLanguage(this.state.selectedLanguage);
+	}
 	updateLanguage(lang){
 		this.setState(function(){
 			return{
 				selectedLanguage: lang
 			}
 		});
+		api.fetchPopularRepos(lang).then(function(repos){
+			this.setState(function(){
+				return {
+					repos: repos
+				}
+			})
+		}.bind(this));
 	}
 	render() {
 		return(
 			<div>
 				<SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage}/>
+				{!this.state.repos ? <p>Loading</p> : <RepoGrids repos={this.state.repos}/> }
 			</div>)
 	}
 }
